8aa2e7383889e92b47577f8c3d59c79458fa55d6
Divya <111332116+divya-gr@users.noreply.github.com>
2023-09-11 17:38:37 +0530

    Update backend.tf

--- module/backend.tf
+++ module/backend.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "gcs" {
-    bucket         = "divya-practise-bucket"
-    credentials =    "./credentials.json"
-    # prefix         = "terraform/state" # Optional: Use a prefix if needed
-  }
-}
+
